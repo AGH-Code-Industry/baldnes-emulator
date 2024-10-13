65e761491f53aaa8994270564686e2b5bb0a88f6
Stanisław Borowy <stabor@startmail.com>
2024-10-13 19:57:08 +0200

    start working on cartridge

--- emulator/src/main.rs
+++ emulator/src/main.rs
@@ -2,10 +2,6 @@ mod addressing;
 mod bus;
 mod empty_device;
 mod memory;
+mod cartridge;
 
-
-fn main() {
-    // let mut bus = Bus::new();
-    // let memory = Memory::new(0x2000);
-    // bus.register(memory, AddressRange::new(0, 0x1FFF));
-}
+fn main() -> anyhow::Result<()> {}
