91d9e629892e84191905a46c46d9457583e57481
HikariIT <alice.it.mail@gmail.com>
2024-12-02 22:04:34 +0100

    [RUW-11] Added Palette RAM

--- emulator/src/ppu/mod.rs
+++ emulator/src/ppu/mod.rs
@@ -1,2 +1,4 @@
 pub mod ppu;
 mod registers;
+pub mod vram;
+pub mod palette_ram;
--- emulator/tests/int_ppu.rs
+++ emulator/tests/int_ppu.rs
@@ -4,8 +4,8 @@ mod tests {
 
     #[test]
     fn test_ppu_vram_write() {
-        emulator::logging::nes_logging::init_logging();
-        let vram = emulator::vram::vram::VRAM::new();
+        // emulator::logging::nes_logging::init_logging();
+        let vram = emulator::ppu::vram::vram::VRAM::new();
         let mut ppu_bus = emulator::bus::Bus::new();
         ppu_bus.register(
             vram,
@@ -22,7 +22,31 @@ mod tests {
 
         let vram_data = ppu.read(*&0x2007);
         assert_eq!(vram_data, 0x00);
-        let vram_data = ppu.read(*&0x2007);
-        assert_eq!(vram_data, 0x66);
+        let vram_data_valid = ppu.read(*&0x2007);
+        assert_eq!(vram_data_valid, 0x66);
+    }
+
+    #[test]
+    fn test_ppu_palette_ram_write() {
+        // emulator::logging::nes_logging::init_logging();
+        let palette_ram = emulator::ppu::palette_ram::palette_ram::PaletteRAM::new();
+        let mut ppu_bus = emulator::bus::Bus::new();
+        ppu_bus.register(
+            palette_ram,
+            emulator::addressing::AddressRange::new(0x3F00, 0x3FFF),
+        );
+
+        let mut ppu = emulator::ppu::ppu::PPU::new(ppu_bus);
+        ppu.write(*&0x2006, 0x3F);
+        ppu.write(*&0x2006, 0x2C);
+        ppu.write(*&0x2007, 0b00101001);
+
+        ppu.write(*&0x2006, 0x3F);
+        ppu.write(*&0x2006, 0x2C);
+
+        let color_index = ppu.read(*&0x2007);
+        assert_eq!(color_index, 0x00);
+        let color_index_valid = ppu.read(*&0x2007);
+        assert_eq!(color_index_valid, 0b00101001);
     }
 }
